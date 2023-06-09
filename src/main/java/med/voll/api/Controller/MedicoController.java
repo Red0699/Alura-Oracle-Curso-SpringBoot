8ce65ad44a798db98786916a8a5dc214c0976db5
Nicolas <yncampos@ucundinamarca.edu.co>
2023-06-08 19:49:32 -0500

    Validaciones

--- src/main/java/med/voll/api/Controller/MedicoController.java
+++ src/main/java/med/voll/api/Controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.Controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,7 @@ public class MedicoController {
     @Autowired
     private MedicoRepository medicoRepository;
     @PostMapping
-    public void registrarMedico(@RequestBody DatosRegistroMedico parametro){
+    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico parametro){
         //System.out.println("El request llega correctamete");
         medicoRepository.save(new Medico(parametro));
     }
