89bf3806a86485b5eb2a111d12862f915cd7cb4a
Nicolas <yncampos@ucundinamarca.edu.co>
2023-06-09 21:04:32 -0500

    Generando token de autenticación

--- src/main/java/med/voll/api/Controller/AutenticacionController.java
+++ src/main/java/med/voll/api/Controller/AutenticacionController.java
@@ -2,6 +2,9 @@ package med.voll.api.Controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuarios.DatosAutenticacionUsuario;
+import med.voll.api.domain.usuarios.Usuario;
+import med.voll.api.infra.security.DatosJWT;
+import med.voll.api.infra.security.TokenService;
 import org.aspectj.weaver.patterns.IToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,10 +23,14 @@ public class AutenticacionController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){
-        Authentication token = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(), datosAutenticacionUsuario.clave());
-        authenticationManager.authenticate(token);
-        return ResponseEntity.ok().build();
+        Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(), datosAutenticacionUsuario.clave());
+        var usuarioAuth= authenticationManager.authenticate(authToken);
+        var JWTtoken = tokenService.generarToken((Usuario) usuarioAuth.getPrincipal());
+        return ResponseEntity.ok(new DatosJWT(JWTtoken));
     }
 }
